aaba638764e50336b3ec57ef57c6b6b66db26d6a
hahadaba <35248642+hahadaba@users.noreply.github.com>
2018-07-28 23:10:33 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
 define('APIKEY', '填写你的GOOGLE API KEY');
-define('GJ_CODE', $_COOKIE['gl']?$_COOKIE['gl']:'HK');
+define('GJ_CODE', $gl);
 define('SITE_NAME', '网站名称');
 define('TITLENAME', '标题名称');
 define('EN2DEKEY', '0090909i604wkkfkak');
