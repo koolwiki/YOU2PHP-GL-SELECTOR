780b81f8cbce87c4f24583ede3a8a5fdd6cd3a89
hahadaba <35248642+hahadaba@users.noreply.github.com>
2018-07-28 19:48:23 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', '填写你的GOOGLE API KEY');
+define('GJ_CODE', $_COOKIE['gl']?$_COOKIE['gl']:'HK');
+define('SITE_NAME', '网站名称');
+define('TITLENAME', '标题名称');
+define('EN2DEKEY', '0090909i604wkkfkak');
+define('EMAIL', '你的邮箱');
+?>
---
+++ select_default_gl.php
@@ -0,0 +1,38 @@
+<?php
+/*
+@在浏览器选择YOU2PHP缺省国家和地区
+2018-7-28
+*/
+$gls=array(
+"HK"=>"香港",
+"TW"=>"台湾",
+"JP"=>"日本",
+"KR"=>"韩国",
+"US"=>"美国",
+"FR"=>"法国",
+"UK"=>"英国",
+"DE"=>"德国",
+"IN"=>"印度");
+//可以增加更多的选项
+
+$gl=$_COOKIE['gl'];
+if(!$gl OR !isset($gls[$gl])){$gl='HK';}
+
+$select[]="<select id='select_gl'>";
+foreach($gls as $k=>$v){
+	$is_selected=($k==$gl)?' selected ':'';
+	$select[]="  <option value =\"$k\"{$is_selected}>$v</option>";
+}
+$select[]="</select>";
+echo join("\n",$select);
+?>
+
+<script>
+$(document).ready(function() {
+	$("#select_gl").change(function(){
+		var gl=$(this).children('option:selected').val()
+		document.cookie="gl="+gl;
+		window.location.reload()
+	})
+})
+</script>
